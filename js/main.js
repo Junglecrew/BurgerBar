40572aad8434f71f7cc0d52b1c5064400748e99c
BabylonBwoy <junglecrew@gmail.com>
2017-11-21 00:19:28 +0200

    21.11.2017

--- js/main.js
+++ js/main.js
@@ -1,35 +1,58 @@
   $(document).ready(function() {
 
     $('#fullpage').fullpage({
-      menu: '#menu'
+      menu: '#menu',
+      responsiveHeight: 650
     });
+
+    // Определение ширины слайдера в % в зависимости от количества слайдов
+    // $(function() {
+    //   let slider= $('.slider__list');
+    //   let sliderItemsCount = $('.slider__item').length;
+    //   let sliderWidth = sliderItemsCount * 100 + "%";
+    //   let setSliderWidth = function(element, width) {
+    //     $(element).css(width, sliderWidth);
+    //   }
+
+    //   setSliderWidth(slider, 'width');
+    // });
+
 
 
     // Меню для мобильной версии 
     $('.burger-menu__link').click(
-      function(){
-        $('.nav-mobile').css('right', '0px');
-    })
+      () => {
+        $('.nav-mobile').css('right', '0px'); //меню появляется слева на полный экран
+        $.fn.fullpage.setAllowScrolling(false); // запрет на скрол мышкой при открытом меню
+        $.fn.fullpage.setKeyboardScrolling(false, 'down'); // запрет на скрол с клавиатуры при открытом меню
+      });
+
     $('.nav-mobile__btn').click(
-      function() {
+      () => {
         $('.nav-mobile').css('right', '999px');
-      })
+        $.fn.fullpage.setAllowScrolling(true); // снятие запрета на скрол мышкой
+        $.fn.fullpage.setKeyboardScrolling(true, 'down'); // снятие запрета на скрол клавиатурой
+      });
 
     // Анимация стрелки вниз
     $('.arrow-down__pic').hover(
-     function() {
+     () => {
       $(this).addClass('animated zoomIn'); 
      },
-     function() {
+     () => {
       $(this).removeClass('animated zoomIn'); 
      });
 
     // вертикальная карусель. Секция "Команда" 
     $('.team__member').click(
-     function() {
-      $('.team__member').removeClass('team__member--active');
-      $(this).addClass('team__member--active');
-     });
+     function(e) {
+      e.preventDefault();
+      if ($(this).hasClass('team__member--active')) {
+          $('.team__member').removeClass('team__member--active');
+      } else {
+          $('.team__member').removeClass('team__member--active');
+          $(this).addClass('team__member--active');
+      }});
 
 
     // Горизонтальный слайдер. Секция "Меню"
@@ -39,10 +62,16 @@
         //Проверка, есть ли активный класс у родителя ссылки, чтобы можно было закрыть все элементы.
         if ($(this).parent().hasClass('menu__item--active')) {
           $('.menu__item').removeClass('menu__item--active');
+          $('.menu__item-text').css({
+            'transition': '0',
+            'transition-delay': '0'
+          });
         }
         else {
-          $('.menu__item').removeClass('menu__item--active'); // Убирает у остальных элементов класс активный.
-          $(this).parent().addClass('menu__item--active'); //родителю данной ссылке даёт активный класс.
+          $('.menu__item').removeClass('menu__item--active');
+           // Убирает у остальных элементов класс активный.
+          $(this).parent().addClass('menu__item--active');
+          //родителю данной ссылке даёт активный класс.
         }
 
       })
@@ -51,23 +80,33 @@
     // Слайдер //
 
     $( () => {
-      var  moveSlide = function(container, slideNum) {
-        var items = container.find('.slider__item'),
-            activeSlide = items.filter('.active'),
-            reqItem = items.eq(slideNum),
-            reqIndex = reqItem.index(),
-            list = container.find('.slider__list'),
-            duration = 500;
+      let list = $('.slider__list'),
+          sliderItemsCount = $('.slider__item').length;
+          sliderWidth = sliderItemsCount * 100 + "%";
+          // Определение ширины слайдера в % в зависимости от количества слайдов
+          setSliderWidth = function(element, width) {
+            $(element).css(width, sliderWidth);
+          }
+          // анимация движения
+          moveSlide = function(container, slideNum) {
+            let items = $('.slider__item'),
+                activeSlide = items.filter('.active'),
+                reqItem = items.eq(slideNum),
+                reqIndex = reqItem.index(),
+                duration = 500;
 
-        if (reqItem.length) {
-          list.animate({
-            'left': -reqIndex * 100 + '%'}, duration, () => {
-              activeSlide.removeClass('active');
-              reqItem.addClass('active');
-          });
-        }
-      }
+            if (reqItem.length) {
+              list.animate({
+                'left': -reqIndex * 100 + '%'}, duration, () => {
+                  activeSlide.removeClass('active');
+                  reqItem.addClass('active');
+              });
+            }
+          }
+      setSliderWidth(list, 'width');
 
+
+      // прокрутка при нажатии на кнопки вправо или влево
       $('.arrow').click(function(e){
         e.preventDefault();
 
@@ -87,7 +126,6 @@
         }
 
         reqItem = existedItem.length ? existedItem.index() : edgeItem.index();
-
 
         moveSlide(container, reqItem);
 
